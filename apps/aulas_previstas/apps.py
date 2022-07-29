ac9ef42f6c2fecbdd84dcd1a1fce17ee6fb0d038
Jose <zemaile@gmail.com>
2022-07-29 08:30:03 +0100

    bug fix AulasPrevistasConfig

--- apps/aulas_previstas/apps.py
+++ apps/aulas_previstas/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class AulasPrevistasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aulas_previstas'
+    name = 'apps.aulas_previstas'
+    verbose_name = "Aulas Previstas"
